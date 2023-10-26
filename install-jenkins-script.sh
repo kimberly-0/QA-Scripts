575272f2ca1e6f9919e31503d2cf5c1b1b3f2d7d
Kimberly D <kimberly.dijkmans@outlook.com>
2023-10-26 13:48:02 +0100

    remove new line whitespace

--- install-jenkins-script.sh
+++ install-jenkins-script.sh
@@ -3,13 +3,9 @@ echo "Running script to install Jenkins..."
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y openjdk-11-jre
 sudo bash
-curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key \
-        | sudo tee /usr/share/keyrings/jenkins-keyring.asc > \
-        /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
- https://pkg.jenkins.io/debian binary/ | \
-        sudo tee /etc/apt/sources.list.d/jenkins.list > \
-        /dev/null
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key \ | sudo tee /usr/share/keyrings/jenkins-keyring.asc > \ /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \ https://pkg.jenkins.io/debian binary/ | \ sudo tee /etc/apt/sources.list.d/jenkins.list > \ /dev/null
 sudo apt-get update
 sudo apt-get install -y jenkins
 sudo apt-get install git docker.io
+echo "Completed Jenkins installation script"
